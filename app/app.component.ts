70ed7914ce375080ea9ae1e7279ceb81cd92825b
Michael Busby <michaelb@clearlyinnovative.com>
2017-11-03 23:07:21 -0400

    add login functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from "@angular/core";
 
+import { UserService } from './shared/user/user.service';
+import { User } from './shared/user/user';
+
 @Component({
   selector: "my-app",
-  templateUrl: "app.component.xml",
+  providers: [UserService],
+  templateUrl: "./pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {
-  email = "nativescriptrocks@telerik.com";
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private userService: UserService) {
+    this.user = new User();
+  }
   submit() {
-    alert("You're using: " + this.email);
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this.userService.register(this.user)
+    .subscribe(
+      () => {
+        alert("Your account was successfully created.");
+        this.toggleDisplay();
+      },
+      () => alert("Unfortunately we were unable to create your account.")
+    );
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
   }
 }
